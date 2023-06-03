0772943cc1267d7f2fd30303b6f099640114baf8
Diego724 <133835245+Diego724@users.noreply.github.com>
2023-06-03 00:04:21 -0500

    Add files via upload

---
+++ Suma y Resta.py
@@ -0,0 +1,8 @@
+numero1 = int(input("Digite el primer numero: "))
+numero2 = int(input("Digite el segundo numero: "))
+
+resultado = numero1 + numero2
+resta = numero1 - numero2
+
+print("El resultado de la suma es:", resultado)
+print("El resultado de la resta es: ",resta)
